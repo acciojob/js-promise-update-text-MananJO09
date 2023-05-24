3cf89fcc5284a4a802877b6d84516c978a55895d
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-05-24 22:30:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let promise = new Promise((resolve , reject)=>{
+	setTimeout(()=>{
+		resolve("Hello world!")
+	},1000)
+})
+
+promise.then((text)=>{
+	document.querySelector("#output").innerText = text
+})
